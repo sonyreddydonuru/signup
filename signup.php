61190c2098621d8214007977345096ca7800bfb0
sonyreddydonuru <80306854+sonyreddydonuru@users.noreply.github.com>
2021-03-21 16:48:26 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,8 @@
+<?php
+$firstname=$_POST["firstname"];
+$surname=$_POST["surname"];
+$email=$_POST["email"];
+$mobilenumber=$_POST["mobilenumber"];
+$newpassword=$_POST["newpassword"];
+echo $firstname;
+?>
